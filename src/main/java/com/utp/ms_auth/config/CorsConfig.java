c259b4f632ac148e9719f2925a98770164e073ed
Cliber Castillo <48666090+clibercastillo@users.noreply.github.com>
2026-08-28 15:24:35 -0500

    Add new allowed origin to CORS configuration

--- src/main/java/com/utp/ms_auth/config/CorsConfig.java
+++ src/main/java/com/utp/ms_auth/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(
+            "http://localhost:4200",
+            "https://clibercastillo.github.io"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
